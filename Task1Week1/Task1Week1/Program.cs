cdc4d51c7068085f645fd1a5313386f93cdb467b
Iprusevich <236247242+iprusdev@users.noreply.github.com>
2025-10-06 18:09:55 +0300

    TasksWeek1

---
+++ Task1Week1/Task1Week1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+main();
+void main()
+{
+    while (true)
+    {
+        float FirstNum, SecondNum, result;
+        string Arifmetic;
+        Console.WriteLine("Введите первое число");
+        while (true)
+        {
+            if (float.TryParse(Console.ReadLine(), out FirstNum))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: вы ввели не число!");
+            }
+        }
+        Console.WriteLine("Выберите операцию +,-,/,*");
+        Arifmetic = Console.ReadLine();
+        Console.WriteLine("Введите второе число");
+        while (true)
+        {
+            if (float.TryParse(Console.ReadLine(), out SecondNum))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: вы ввели не число!");
+            }
+        }
+        switch (Arifmetic)
+        {
+            case "+":
+                result = FirstNum + SecondNum;
+                Console.WriteLine($"Результат = {result}");
+                break;
+            case "-":
+                result = FirstNum - SecondNum;
+                Console.WriteLine($"Результат = {result}");
+                break;
+
+            case "*":
+                result = FirstNum * SecondNum;
+                Console.WriteLine($"Результат = {result}");
+                break;
+
+            case "/":
+
+                if (SecondNum != 0)
+                {
+                    result = FirstNum / SecondNum;
+                    Console.WriteLine($"Результат = {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! На ноль делить нельзя");
+                }
+                break;
+            default:
+                Console.WriteLine("Неизвестная операция!");
+                break;
+        }
+
+        Console.WriteLine("Введите число");
+        Console.WriteLine("1. Нажмите 1 если хотите выполнить новую операцию");
+        Console.WriteLine("2. Нажмите любую кнопку чтобы выйти из программы");
+        string quitOrReturn = Console.ReadLine();
+        if (quitOrReturn == "1")
+        {
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine("Заверешение программы");
+            Thread.Sleep(2000);
+            break;
+        }
+    }
+}
---
+++ Task2Week1/Task2Week1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Threading.Thread;
+class File
+{
+    public string ProcessData(string dataName)
+    {
+        Thread.Sleep(3000);
+        return $"Обработка '{dataName}' завершена за 3 секунды";
+    }
+    async public Task<string> ProcessDataAsync(string dataName)
+    {
+        await Task.Delay(3000);
+        return $"Обработка '{dataName}' завершена за 3 секунды";
+    }
+}
+
+class Program
+{
+    static async Task Main()
+    {
+
+        File file1 = new File();
+        File file2 = new File();
+        File file3 = new File();
+        Console.WriteLine(file1.ProcessData("Файл 1"));
+        Console.WriteLine(file2.ProcessData("Файл 2"));
+        Console.WriteLine(file3.ProcessData("Файл 3"));
+
+
+        File file4 = new File();
+        File file5 = new File();
+        File file6 = new File();
+
+        Task<string> task1 = file4.ProcessDataAsync("Файл 4");
+        Task<string> task2 = file5.ProcessDataAsync("Файл 5");
+        Task<string> task3 = file6.ProcessDataAsync("Файл 6");
+
+        string[] results = await Task.WhenAll(task1, task2, task3);
+
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
+
