2e29da81b61119080687ac94cbc074ff62acaccd
Iprusevich <236247242+iprusdev@users.noreply.github.com>
2025-10-21 02:04:16 +0300

    Update InMemoryData.cs

--- Task4Week3/Task4Week3/Data/InMemoryData.cs
+++ Task4Week3/Task4Week3/Data/InMemoryData.cs
@@ -7,14 +7,16 @@ namespace Task4Week3.Data
         public static readonly List<Author> Authors = new List<Author>
         {
             new Author { Id = 1, Name = "Лев Толстой", DateOfBirth = new DateTime(1828, 9, 9) },
-            new Author { Id = 2, Name = "Фёдор Достоевский", DateOfBirth = new DateTime(1821, 11, 11) }
+            new Author { Id = 2, Name = "Фёдор Достоевский", DateOfBirth = new DateTime(1821, 11, 11) },
+            new Author { Id = 3, Name = "Дж.К. Роулинг", DateOfBirth = new DateTime(1965, 31, 5)}
         };
 
         public static readonly List<Book> Books = new List<Book>
         {
             new Book { Id = 1, Title = "Война и мир", PublishedYear = 1869, AuthorId = 1 },
             new Book { Id = 2, Title = "Анна Каренина", PublishedYear = 1877, AuthorId = 1 },
-            new Book { Id = 3, Title = "Преступление и наказание", PublishedYear = 1866, AuthorId = 2 }
+            new Book { Id = 3, Title = "Преступление и наказание", PublishedYear = 1866, AuthorId = 2 },
+            new Book { Id = 3, Title = "Гарри Поттер и философский камень", PublishedYear = 1997, AuthorId = 3}
         };
     }
 }
